369254d62474f79725b98c7469b70f55aa40062b
Mike Sperber <sperber@deinprogramm.de>
2025-04-03 14:22:29 +0200

    Update.

--- src/main/scala/contract/Contract.scala
+++ src/main/scala/contract/Contract.scala
@@ -8,6 +8,15 @@ package contract
 
 case class Date(iso: String)
 
+type Amount = Double
+
+enum Currency {
+  case EUR
+  case GBP
+  case USD
+  case YEN
+}
+
 enum Contract {
   case ZeroCouponBond(date: Date, amount: Amount, currency: Currency)
 }
