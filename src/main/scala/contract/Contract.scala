822523b238e5e3fcc517405a51e0475432cd0e00
Mike Sperber <sperber@deinprogramm.de>
2025-04-03 15:06:35 +0200

    Update.

--- src/main/scala/contract/Contract.scala
+++ src/main/scala/contract/Contract.scala
@@ -37,4 +37,8 @@ val c1 = One(Currency.EUR)
 val c2 = More(100, One(Currency.EUR))
 
 val zcb1 = Timed(Date("2025-12-24"), More(100, One(EUR)))
+
+def zeroCouponBond(date: Date, amount: Amount, currency: Currency): Contract =
+  Timed(date, More(amount, One(currency)))
+
 // val zcb1 = ZeroCouponBond(Date("2025-12-24"), 100, Currency.EUR)
