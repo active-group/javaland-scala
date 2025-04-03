b0fe659086b306f0c241aee0db38c74b788fe408
Mike Sperber <sperber@deinprogramm.de>
2025-04-03 14:21:30 +0200

    Update.

--- src/main/scala/contract/Contract.scala
+++ src/main/scala/contract/Contract.scala
@@ -1,3 +1,11 @@
 package contract
 
+/*
+ * 1. einfaches Beispiel
+ *    Zero-Bond / zero-coupon bond
+ *    "Ich bekomme am 24.12.2025 100€."
+*/
 
+enum Contract {
+  case ZeroCouponBond(date: Date, amount: Amount, currency: Currency)
+}
